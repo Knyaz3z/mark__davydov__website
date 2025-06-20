c6f3a95cd69fcee29067f499105910629d171aa2
Max <knyaz010official@gmail.com>
2025-06-20 16:25:49 +0300

    возврат

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -101,7 +101,7 @@ function MainHero1About() {
 function MainHero2Lines(){
         const connections = [
             {
-                source: "Учись111",
+                source: "Учись",
                 target: "Всегда чему-то новому, достигай новых вершин"
             },
             {
